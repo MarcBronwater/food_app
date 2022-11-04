54ae0404e3b59f60026ebb73b95dfc8ba7cf19d1
MarcBronwater <92919332+MarcBronwater@users.noreply.github.com>
2022-11-04 15:02:49 +0100

    Update Results.py

--- pages/Results.py
+++ pages/Results.py
@@ -20,6 +20,38 @@ Duration = variables_list[1]
 Health_goal = variables_list[2]
 Diet = variables_list[3]
 
+# create navigation
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
+                
+if st.button('Go to main'):
+    nav_page("Main_page")
+    
+if st.button('Change preferences'):
+    nav_page("questionnaire")
+
 Model = tf.keras.models.load_model("model.h5")
 
 
@@ -78,32 +110,5 @@ for i in range(len(df_recipes)):
             for k in j:
                 st.success(df_recipes.loc[k, 'name'])
 
-# create navigation
-def nav_page(page_name, timeout_secs=3):
-    nav_script = """
-        <script type="text/javascript">
-            function attempt_nav_page(page_name, start_time, timeout_secs) {
-                var links = window.parent.document.getElementsByTagName("a");
-                for (var i = 0; i < links.length; i++) {
-                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
-                        links[i].click();
-                        return;
-                    }
-                }
-                var elasped = new Date() - start_time;
-                if (elasped < timeout_secs * 1000) {
-                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
-                } else {
-                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
-                }
-            }
-            window.addEventListener("load", function() {
-                attempt_nav_page("%s", new Date(), %d);
-            });
-        </script>
-    """ % (page_name, timeout_secs)
-    html(nav_script)
-                
-if st.button('Go to main'):
-    nav_page("Main_page")
+
 
